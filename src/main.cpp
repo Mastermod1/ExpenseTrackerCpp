98b797bf4721c453ab181b3a55ee71b7a99b066c
Mastermod1 <danielkalmucki2@gmail.com>
2023-05-03 12:55:49 +0200

    Basic setup with some main function

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdlib.h>
+
+#if __linux__
+    #define CLEAR "clear"
+#else
+    #define CLEAR "cls"
+#endif
+
+void clearConsole()
+{
+    std::system(CLEAR);
+}
+
+bool menu()
+{
+    std::cout << "----MENU----\n"; 
+    std::cout << "  1. Add expense\n";
+    std::cout << "  2. Show raport\n";
+    std::cout << "  3. Exit\n";
+    
+    int option = 0;
+    std::cin >> option;
+
+    if (option == 3)
+    {
+	return false;
+    }
+    return true;
+}
+
+int main()
+{
+    clearConsole();
+    while(menu())
+    {
+	clearConsole();
+    }
+}
---
+++ test/gtest_test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(FirstTestShould, AssertSth)
+{
+    EXPECT_EQ(5, 5);
+}
